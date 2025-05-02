dea6dc56963a50f43476269d7e856d9053739d3d
licoded <busy.li@foxmail.com>
2025-05-02 10:06:51 +0800

    vue-config: explictly config bundler build feature flags

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,16 @@ const { defineConfig } = require('@vue/cli-service')
 // const fs = require('fs')
 // const path = require('path')
 module.exports = defineConfig({
+  chainWebpack: (config) => {
+    config.plugin('define').tap((definitions) => {
+      Object.assign(definitions[0], {
+        __VUE_OPTIONS_API__: 'true',
+        __VUE_PROD_DEVTOOLS__: 'false',
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'true',
+      });
+      return definitions
+    })
+  },
   transpileDependencies: true,
   lintOnSave: false,
   // devServer: {
